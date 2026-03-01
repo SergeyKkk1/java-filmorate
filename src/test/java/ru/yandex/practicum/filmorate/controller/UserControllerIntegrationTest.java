342635a3c53f56cc05698d0ae127ae40bf28af5d
sergey <sergey.kobelew@yandex.ru>
2026-03-01 22:02:03 +0300

    Films users basic controller implementations

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerIntegrationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerIntegrationTest.java
@@ -1,16 +1,16 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.yandex.practicum.filmorate.dto.UserDto;
 import ru.yandex.practicum.filmorate.service.UserService;
-import tools.jackson.core.type.TypeReference;
-import tools.jackson.databind.ObjectMapper;
 
 import java.time.LocalDate;
 import java.util.List;
